8e7e36f025d80e2fdb94bb9db5168d95e4b69a38
Julian Bustos <julian.m.bustos@gmail.com>
2023-11-28 17:47:01 -0500

    ads the key

--- app/admin/dashboard/markets/_components/MarketDays.tsx
+++ app/admin/dashboard/markets/_components/MarketDays.tsx
@@ -7,9 +7,9 @@ const MarketDays = ({ dates }: { dates: string[] }) => {
   const [selectedDay, setSelectedDay] = useState<string |  null>(null);
   return (
     <div className="flex gap-4 w-fit mx-auto">
-      {dates.map((date) => {
+      {dates.map((date, i) => {
         return (
-          <button type="button" onClick={() => setSelectedDay(date)} className={`flex flex-col justify-center gap-4 w-fit border-4 border-black p-2 rounded-[20px] ${selectedDay === date ? "border-black" : "border-slate-200"} `}>
+          <button key={date + i} type="button" onClick={() => setSelectedDay(date)} className={`flex flex-col justify-center gap-4 w-fit border-4 border-black p-2 rounded-[20px] ${selectedDay === date ? "border-black" : "border-slate-200"} `}>
             <h2 className="font-bold text-xl max-w-[8ch] text-center">{formatMarketDate(date).replace(",", "")}</h2>
           </button>
         );
