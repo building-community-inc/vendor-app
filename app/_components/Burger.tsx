ba0e7e87a8f6beff952b0a210fb75dde44aa5167
Julian Bustos <julian.m.bustos@gmail.com>
2023-12-04 13:31:03 -0500

    ads the burger and mobile nav to the admin dashboard

---
+++ app/_components/Burger.tsx
@@ -0,0 +1,51 @@
+
+interface BurgerProps {
+  onClickHandler?: React.MouseEventHandler<HTMLDivElement>;
+  isNavOpen: boolean;
+  barColor?: string;
+}
+
+const Burger = ({ onClickHandler, isNavOpen, barColor }: BurgerProps) => {
+  return (
+    <div className="flex justify-center items-center" onClick={onClickHandler}>
+      <div className="relative grid place-content-center z-50 gap-2 cursor-pointer ">
+        <Bar
+          rotateZ={!isNavOpen ? "rotate-0" : "rotate-45 translate-y-3"}
+          barColor={barColor}
+          width="w-7"
+        />
+        <Bar
+          width={isNavOpen ? "w-0" : "w-7"}
+          barColor={barColor}
+        />
+        <Bar
+          rotateZ={!isNavOpen ? "rotate-0" : "-rotate-45 -translate-y-2"}
+          barColor={barColor}
+          width="w-7"
+        />
+      </div>
+    </div>
+  );
+};
+interface BarProps {
+  width?: string;
+  rotateZ?:
+    | "rotate-45 translate-y-3"
+    | "-rotate-45 -translate-y-2"
+    | "rotate-0";
+  barColor?: string;
+}
+const Bar = ({
+  width = "w-10",
+  rotateZ = "rotate-0",
+  barColor = "bg-black",
+}: BarProps) => {
+  return (
+    <div
+      className={`${width} ${rotateZ} transition-all h-[2px]  ${barColor} rounded-md`}
+      style={{ transformOrigin: 'center' }}
+
+    />
+  );
+};
+export default Burger;
--- app/admin/dashboard/_components/AdminNavbar.tsx
+++ app/admin/dashboard/_components/AdminNavbar.tsx
@@ -5,60 +5,83 @@ import navOptions from "./navOptions.json";
 import { SignOutButton } from "@clerk/nextjs";
 import { TUserWithOptionalBusinessRef } from "@/zod/user-business";
 import { usePathname } from "next/navigation";
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+import Burger from "@/app/_components/Burger";
 
 const AdminNavbar = ({ user }: { user: TUserWithOptionalBusinessRef }) => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   const pathname = usePathname();
   // console.log({ pathname });
-  return (
-    <aside className="bg-nav-bg shadow-[10px_3px_6px_#00000029] text-nav-text px-10 flex flex-col py-[10px] gap-2 h-screen overflow-y-scroll hide-scrollbar pb-10">
-      <Link href="/">
-        <Image
-          src={"/logo-on-white-bg.png"}
-          alt="logo"
-          width={468}
-          height={468}
-        />
-      </Link>
 
-      {navOptions.map((option) => (
-        <React.Fragment key={option.href}>
-          {option.title === "Logout" ? (
-            <div className="text-center uppercase font-bold text-xl">
-              <SignOutButton key={option.href} />
-            </div>
-          ) : (
-            <div className="flex flex-col items-center mb-2">
-              <Link
-                href={option.href}
-                key={option.title}
-                className="text-center uppercase font-bold text-xl mb-4 relative"
-              >
-                {pathname === option.href && (
-                  <span className="absolute -left-5 top-1/2 -translate-y-1/2">{"->"}</span>
-                )}
-                {option.title}
-              </Link>
-              {option.links && (
-                <>
-                  {option.links.map((link) => (
-                    <Link
-                      href={link.href}
-                      key={link.title}
-                      className="text-center relative"
-                    >
-                      {pathname === link.href && (
-                        <span className="absolute -left-4">{"->"}</span>
-                      )}
-                      {link.title}
-                    </Link>
-                  ))}
-                </>
+
+  useEffect(() => {
+    setIsNavOpen(false);
+  }, [pathname])
+  return (
+    <aside
+      className={`absolute z-10 md:relative text-nav-text px-10 flex flex-col py-[10px] gap-2 h-screen overflow-y-scroll hide-scrollbar pb-10 ${
+        isNavOpen ? "w-[360px] shadow-[10px_3px_6px_#00000029] bg-nav-bg" : "w-0 bg-none"
+      } transition-all duration-200 ease-in-out md:w-[360px] md:shadow-[10px_3px_6px_#00000029]`}
+    >
+      <div className="absolute top-4 left-5 md:hidden">
+        <Burger
+          isNavOpen={isNavOpen}
+          onClickHandler={() => setIsNavOpen(!isNavOpen)}
+        />
+      </div>
+      {/* {isNavOpen && ( */}
+        <div className={`${isNavOpen ? "" : "hidden"} md:block`}>
+          <Link href="/">
+            <Image
+              src={"/logo-on-white-bg.png"}
+              alt="logo"
+              width={468}
+              height={468}
+            />
+          </Link>
+
+          {navOptions.map((option) => (
+            <React.Fragment key={option.href}>
+              {option.title === "Logout" ? (
+                <div className="text-center uppercase font-bold text-xl">
+                  <SignOutButton key={option.href} />
+                </div>
+              ) : (
+                <div className="flex flex-col items-center mb-2">
+                  <Link
+                    href={option.href}
+                    key={option.title}
+                    className="text-center uppercase font-bold text-xl mb-4 relative"
+                  >
+                    {pathname === option.href && (
+                      <span className="absolute -left-5 top-1/2 -translate-y-1/2">
+                        {"->"}
+                      </span>
+                    )}
+                    {option.title}
+                  </Link>
+                  {option.links && (
+                    <>
+                      {option.links.map((link) => (
+                        <Link
+                          href={link.href}
+                          key={link.title}
+                          className="text-center relative"
+                        >
+                          {pathname === link.href && (
+                            <span className="absolute -left-4">{"->"}</span>
+                          )}
+                          {link.title}
+                        </Link>
+                      ))}
+                    </>
+                  )}
+                </div>
               )}
-            </div>
-          )}
-        </React.Fragment>
-      ))}
+            </React.Fragment>
+          ))}
+        </div>
+      {/* )} */}
     </aside>
   );
 };
