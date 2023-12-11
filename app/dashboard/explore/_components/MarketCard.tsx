a7982c833501def780384d4bc0463b376ad870a1
Julian Bustos <julian.m.bustos@gmail.com>
2023-12-11 13:25:16 -0500

    undoes update

--- app/dashboard/explore/_components/MarketCard.tsx
+++ app/dashboard/explore/_components/MarketCard.tsx
@@ -21,7 +21,6 @@ const MarketCard = ({ market }: { market: TSanityMarket }) => {
       <h3 className="text-2xl font-bold font-inter">{market.name}</h3>
       <p className="font-roboto">{market.description}</p>
       <p className="font-roboto">{dateToDisplay}</p>
-      <p className="font-roboto">{market.venue.hours}</p>
     </li>
   );
 };
