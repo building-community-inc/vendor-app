67ec19149f92c241e9b69675ce0d22d5296ef8bf
Julian Bustos <julian.m.bustos@gmail.com>
2023-12-12 09:55:09 -0500

    tests name file build error issue

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { getSanityUserByEmail } from "@/sanity/queries/user";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import VendorNavBar from "./_components/VendorNavBar";
+import VendorNavBar from "./_components/VendorNavBar-copy";
 
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
@@ -15,7 +15,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <section className="flex  h-screen overflow-y-hidden">
       <VendorNavBar user={sanityUser} />
-      
+
       <div className="h-full overflow-y-scroll w-full hide-scrollbar pb-5">
         {children}
       </div>
