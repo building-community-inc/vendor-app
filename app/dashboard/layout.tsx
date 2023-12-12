c41cecacc761398541324a272023a3b42b1fdb89
Julian Bustos <julian.m.bustos@gmail.com>
2023-12-12 10:05:09 -0500

    changes the name of the file to test build

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import { getSanityUserByEmail } from "@/sanity/queries/user";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import VendorNavBar from "./_components/VendorNavBar";
+import NavBar from "./_components/NavBar";
 
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
@@ -14,7 +14,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <section className="flex  h-screen overflow-y-hidden">
-      <VendorNavBar user={sanityUser} />
+      <NavBar user={sanityUser} />
 
       <div className="h-full overflow-y-scroll w-full hide-scrollbar pb-5">
         {children}
