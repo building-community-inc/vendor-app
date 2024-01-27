21f969ebbf516e0b8a02b90cbcfc796d9bf5f803
Julian Bustos <julian.m.bustos@gmail.com>
2024-01-27 12:33:32 -0500

    Refactor searchParams in Page component

--- app/dashboard/checkout/page.tsx
+++ app/dashboard/checkout/page.tsx
@@ -1,13 +1,9 @@
 import Checkout from "./_components/Checkout";
 
-const Page = ({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) => {
+const Page = () => {
   return (
     <main className="min-h-full flex flex-col items-center justify-center">
-      <Checkout searchParams={searchParams} />
+      <Checkout />
     </main>
   );
 }
