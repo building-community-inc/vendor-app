c61d7f2bf60ec27f45fb01575bc928f1c91878dc
Julian Bustos <julian.m.bustos@gmail.com>
2024-02-01 10:34:58 -0500

    renders payment information

--- app/dashboard/checkout/create-later-payment-intent/success/page.tsx
+++ app/dashboard/checkout/create-later-payment-intent/success/page.tsx
@@ -1,7 +1,58 @@
-const Page = () => {
+import { getPaymentByIdWithMarket } from "@/sanity/queries/payments";
+import { formatMarketDate } from "@/utils/helpers";
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: {
+    [key: string]: string | undefined;
+  };
+}) => {
+
+  if (!searchParams.paymentId) {
+    return (
+      <main>
+        <h1>Payment Not Found</h1>
+      </main>
+    );
+  }
+
+  const payment = await getPaymentByIdWithMarket(searchParams.paymentId);
   return (
-    <main>
-      <h1>Payment Successful</h1>
+    <main className="flex flex-col items-center h-full justify-center">
+      <h1 className="font-bold text-xl">Payment Successful</h1>
+
+      <p><strong>Order Id:</strong>{payment._id}</p>
+      <p><strong>Market:</strong>{payment.market.name}</p>
+      <p><strong>Amount Paid:</strong>${payment.amount.paid}</p>
+      <p><strong>Amount Owed:</strong>${payment.amount.owed}</p>
+      <p><strong>Amount Total:</strong>${payment.amount.total}</p>
+      <ul>
+        <h2 className="font-bold text-lg">Payments</h2>
+        {payment.payments.map((payment, index) => (
+          <li key={payment.paymentDate} className="flex items-start gap-2">
+            <span>{index + 1}.</span>
+            <div>
+
+              <p><strong>Date:</strong>{formatMarketDate(payment.paymentDate)}</p>
+              <p><strong>Amount:</strong>${payment.amount}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        <h2 className="font-bold text-lg">Tables Booked</h2>
+        {payment.items.map((item, index) => (
+          <li key={item.date} className="flex items-start gap-2">
+            <span>{index + 1}.</span>
+            <div>
+
+              <p><strong>Date:</strong>{formatMarketDate(item.date)}</p>
+              <p><strong>Table Id:</strong>{item.tableId}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
--- sanity/queries/payments.ts
+++ sanity/queries/payments.ts
@@ -10,41 +10,85 @@ export const getExistingPayment = async (paymentIntentId: string) => {
   return result;
 };
 
-
 export const zodLatePaymentSchema = z.object({
   _id: z.string(),
-  payments: z.array(z.object({
-    _key: z.string(),
-    paymentDate: z.string(),
-    stripePaymentIntentId: z.string(),
-    amount: z.number(),
-    _type: z.string()
-  })),
+  payments: z.array(
+    z.object({
+      _key: z.string(),
+      paymentDate: z.string(),
+      stripePaymentIntentId: z.string(),
+      amount: z.number(),
+      _type: z.string(),
+    })
+  ),
   amount: z.object({
     paid: z.number(),
     total: z.number(),
-    owed: z.number()
-  })
-})
+    owed: z.number(),
+  }),
+});
+export const zodLatePaymentWithMarketSchema = z.object({
+  _id: z.string(),
+  payments: z.array(
+    z.object({
+      _key: z.string(),
+      paymentDate: z.string(),
+      stripePaymentIntentId: z.string(),
+      amount: z.number(),
+      _type: z.string(),
+    })
+  ),
+  amount: z.object({
+    paid: z.number(),
+    total: z.number(),
+    owed: z.number(),
+  }),
+  market: z.object({
+    name: z.string(),
+    _id: z.string(),
+  }),
+  items: z.array(
+    z.object({
+      tableId: z.string(),
+      date: z.string(),
+      price: z.number(),
+    })
+  ),
+});
+
+const paymentQuery = `
+  _id,
+  "payments": payments [] {
+    _key,
+    paymentDate,
+    stripePaymentIntentId,
+    amount,
+    _type
+  },
+  "amount": amount {
+    paid,
+    total,
+    owed
+  },
+`;
+
+const paymentQueryWithMarket = `
+  ${paymentQuery}
+  "market": market->{
+    name,
+    _id
+  },
+  "items": items[] {
+    tableId,
+    date,
+    price
+  }
+`;
 
 export type TLatePaymentSchema = z.infer<typeof zodLatePaymentSchema>;
 export const getPaymentById = async (paymentId: string) => {
   const result = await sanityClient.fetch(
-    `*[_type == "paymentRecord" && _id match $paymentId][0]{
-      _id,
-      "payments": payments [] {
-        _key,
-        paymentDate,
-        stripePaymentIntentId,
-        amount,
-        _type
-      },
-      "amount": amount {
-        paid,
-        total,
-        owed
-      }
-    }`,
+    `*[_type == "paymentRecord" && _id match $paymentId][0] {${paymentQuery}}`,
     { paymentId }
   );
 
@@ -54,15 +98,24 @@ export const getPaymentById = async (paymentId: string) => {
     throw new Error(parsedResult.error.message);
   }
 
+  return parsedResult.data;
+};
+export const getPaymentByIdWithMarket = async (paymentId: string) => {
+  const result = await sanityClient.fetch(
+    `*[_type == "paymentRecord" && _id match $paymentId][0] {${paymentQueryWithMarket}}`,
+    { paymentId }
+  );
+  const parsedResult = zodLatePaymentWithMarketSchema.safeParse(result);
+
+  if (!parsedResult.success) {
+    throw new Error(parsedResult.error.message);
+  }
 
   return parsedResult.data;
 };
 
-
 export const getAllExistingPayment = async () => {
-  const result = await sanityClient.fetch(
-    '*[_type == "paymentRecord"]'
-  );
+  const result = await sanityClient.fetch('*[_type == "paymentRecord"]');
 
   return result;
-}
+};
