7cd2f2184c688b65b84bd8fefaf60b504fe124d8
Julian Bustos <julian.m.bustos@gmail.com>
2024-02-21 20:43:11 -0500

    fixes the vendors not having pending

--- sanity/seeds/addsAprovedToDev.js
+++ sanity/seeds/addsAprovedToDev.js
@@ -3,8 +3,6 @@ const { createClient } = require("next-sanity");
 
 const client = createClient({
   projectId: "xomfbfrw",
-  dataset: "development",
-  token: "skQn2uk2XttOketiUngOA2HhdZd5mlov60h8fjut2RSTVst5miLf4r8NVX9xFaVKahQ5oXi6E2FXT3djKxUAgDOxkOMePyxMEm6sEmwmDY0FWsGWUknrzDXCD2CBgwYh1bnW4YVIWaxKxlx6GhOteDy9kwBaCIQtbM0E8SdGk0EvSHlNMsy0", // Optional if your dataset is public
   useCdn: false, // Set to true if you want to use the CDN
 });
 
