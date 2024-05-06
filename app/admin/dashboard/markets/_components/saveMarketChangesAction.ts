e42dcb67814969eda0eef4e7d1fbd8bdf81872e4
Julian Bustos <julian.m.bustos@gmail.com>
2024-05-06 09:52:43 -0400

    gets the days information ready to be stored

--- app/admin/dashboard/markets/_components/saveMarketChangesAction.ts
+++ app/admin/dashboard/markets/_components/saveMarketChangesAction.ts
@@ -1,5 +1,5 @@
 "use server";
-import { sanityClient } from "@/sanity/lib/client";
+import { sanityClient, sanityWriteClient } from "@/sanity/lib/client";
 import { areDatesSame } from "@/utils/helpers";
 import { z } from "zod";
 
@@ -18,7 +18,7 @@ const zodRefObject = z.object({
 
 const tableObjectSchema = z.object({
   table: tableSchema,
-  booked: zodRefObject,
+  booked: zodRefObject.optional().nullable(),
 });
 
 const daySchema = z.object({
@@ -80,11 +80,13 @@ export const saveMarketChanges = async (
 
   const sanityMarket = await sanityClient.fetch(
     `*[_type == "market" && _id == "${parsedData.data.marketId}"][0] {
+      _id,
       "daysWithTables": daysWithTables[] {
         date,
         "tables": tables[] {
-          "id": table.id,
-          "price": table.price,
+          "table": table {
+            ...,
+          },
           "booked": booked -> {
             "_type": "reference",
             "_ref": _id
@@ -100,14 +102,19 @@ export const saveMarketChanges = async (
       success: false,
       error: "Error finding market",
     };
+
+  console.log({ table: sanityMarket.daysWithTables[0].tables[0] });
   const zodSanityMarket = z.object({
+    _id: z.string(),
     daysWithTables: z.array(
       z.object({
         date: z.string(),
         tables: z.array(
           z.object({
-            id: z.string(),
-            price: z.number(),
+            table: z.object({
+              id: z.string(),
+              price: z.number(),
+            }),
             booked: zodRefObject.optional().nullable(),
           })
         ),
@@ -137,17 +144,37 @@ export const saveMarketChanges = async (
 
   const dayWithTables: TDaySchema = {
     date: parsedData.data.date,
-    tables: vendors.map((vendor) => ({
-      table: {
-        id: vendor.tableSelection,
-        price: 0,
-      },
-      booked: {
-        _type: "reference",
-        _ref: vendor.vendorId,
-      },
-    })),
+    tables: dayToUpdate.tables.map((table) => {
+      const vendor = vendors.find(
+        (vendor) => vendor.tableSelection === table.table.id
+      );
+
+      if (vendor) {
+        return {
+          ...table,
+          table: {
+            ...table.table,
+            price: table.table.price || 0,
+          },
+          booked: {
+            _type: "reference",
+            _ref: vendor.vendorId,
+          },
+        };
+      } else
+        return {
+          ...table,
+          booked: null,
+        };
+    }),
   };
+
+  if (dayWithTables.tables.some((table) => table.table.price === 0)) {
+    return {
+      success: false,
+      error: "error finding prices",
+    };
+  }
 
   if (!areDatesSame(dayToUpdate.date, dayWithTables.date)) {
     return {
@@ -156,27 +183,59 @@ export const saveMarketChanges = async (
     };
   }
 
+  // if (!dayToUpdate.tables) {
+  //   return {
+  //     success: false,
+  //     error: "no tables found"
+  //   }
+  // }
+
+  if (!areAllTablesInTable2InTable1(dayToUpdate.tables, dayWithTables.tables)) {
+    return {
+      success: false,
+      error: "issue with the tables",
+    };
+  }
+
   const areTablesDifferent = areTableArraysDifferent(
-    dayToUpdate.tables.map((table) => ({
-      table: {
-        id: table.id,
-        price: table.price
-      },
-      booked: table.booked,
-    })),
+    dayToUpdate.tables,
     dayWithTables.tables
   );
 
+  if (!areTablesDifferent) {
+    return {
+      success: false,
+      error: "tables are the same, update one to save changes",
+    };
+  }
+
+  const updatedDaysWithTables = parsedSanityMarket.data.daysWithTables.map(
+    (day) => {
+      if (areDatesSame(day.date, dayWithTables.date)) {
+        return dayWithTables;
+      } else {
+        return day;
+      }
+    }
+  );
+
+  // const newTables:
   console.log({
-    // vendors,
-    // tableSelections,
     areTablesDifferent,
-    // date: rawData.date,
-    // marketId: rawData.marketId,
-    // sanityMarket,
-    dayWithTables,
-    dayToUpdate,
+    updatedDaysWithTables,
+    newTables: updatedDaysWithTables[0].tables,
+    oldTables: dayToUpdate.tables,
   });
+
+
+  // const sanityResp = await sanityWriteClient
+  // .patch(parsedSanityMarket.data._id) // replace '<document-id>' with the id of the document you want to update
+  // .set({
+  //   'daysWithTables': updatedDaysWithTables // replace 'updatedDaysWithTables' with the updated array
+  // })
+  // .commit() // Don't forget to call commit() to send the patch
+
+
 
   return {
     success: false,
@@ -184,11 +243,11 @@ export const saveMarketChanges = async (
   };
 };
 
-type Table = {
+type TTable = {
   table: {
     id: string;
   };
-  booked:
+  booked?:
     | {
         _type: string;
         _ref: string;
@@ -198,23 +257,32 @@ type Table = {
     | undefined;
 };
 
-function areTableArraysDifferent(tables1: Table[], tables2: Table[]): boolean {
-  for (let table1 of tables1) {
-    const table2 = tables2.find(t => t.table.id === table1.table.id);
-    if (table2) {
-      if (table1.booked?._ref !== table2.booked?._ref) {
-        return true;
-      }
-    } else {
-      return true; // table1 is not in tables2
-    }
-  }
+function areTableArraysDifferent(
+  oldTables: TTable[],
+  newTables: TTable[]
+): boolean {
+  for (const newTable of newTables) {
+    const oldTable = oldTables.find((t) => t.table.id === newTable.table.id);
 
-  for (let table2 of tables2) {
-    if (!tables1.find(t => t.table.id === table2.table.id)) {
-      return true; // table2 is not in tables1
+    if (!oldTable) {
+      // console.log("newTable is not in oldTable");
+      return true; // newTable is not in oldTable
+    }
+
+    if (oldTable.booked?._ref !== newTable.booked?._ref) {
+      // console.log("booked refs are not the same");
+      return true;
     }
   }
 
   return false;
+}
+
+function areAllTablesInTable2InTable1(
+  tables1: TTable[],
+  tables2: TTable[]
+): boolean {
+  return tables2.every((table2) =>
+    tables1.some((table1) => table1.table.id === table2.table?.id)
+  );
 }
