383fec827c0469e8fb348d7c4e6170c520e91049
Julian Bustos <julian.m.bustos@gmail.com>
2024-10-24 14:35:46 -0400

    add web app manifest and favicons for improved branding and PWA support

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,11 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <script defer data-domain="vendorapp.buildingcommunityinc.com" src="https://plausible.io/js/script.js"></script>
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+          <link rel="manifest" href="/site.webmanifest" />
         </head>
         <body className={`bg-background text-primary ${inter.className} ${roboto.className} ${segoe.className} ${darkerGrotesque.className}`}>{children}</body>
       </html>
