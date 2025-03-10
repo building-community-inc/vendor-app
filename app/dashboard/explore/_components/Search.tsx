d58f4c29fe04d10f96e244266c226a855d910b88
Julian Bustos <julian.m.bustos@gmail.com>
2025-03-09 20:16:39 -0400

    syncs the search input with the search params

--- app/dashboard/explore/_components/Search.tsx
+++ app/dashboard/explore/_components/Search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/utils";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Search = ({
   urlForSearch,
@@ -12,8 +12,9 @@ const Search = ({
   theme?: "dark" | "light";
   placeholder?: string;
 }) => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search");
   const { push } = useRouter();
-
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
     push(`${urlForSearch}?search=${value}`);
@@ -24,6 +25,7 @@ const Search = ({
       type="text"
       placeholder={placeholder}
       onChange={handleSearch}
+      value={search || ''}
       className={cn(
         `bg-background text-black border border-button-border-color rounded-full px-3 py-2 w-[100%]`,
         theme === "light" && "bg-white text-black border border-black"
