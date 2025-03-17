021ea6bb0833c01764ae77471d37f4665e41051a
Julian Bustos <julian.m.bustos@gmail.com>
2025-03-17 18:52:32 -0400

    fix: update authorized parties in authentication middleware to remove trailing slash

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,7 @@ const publicRoutes = [
 
 export default authMiddleware({
   publicRoutes,
-  authorizedParties: ["https://vendorapp.buildingcommunityinc.com/", "http://localhost:3000"]
+  authorizedParties: ["https://vendorapp.buildingcommunityinc.com", "http://localhost:3000"]
 });
 
 export const config = {
