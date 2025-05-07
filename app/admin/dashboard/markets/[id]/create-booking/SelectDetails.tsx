ebf126ec3d03e4c8d6b89b64ec82e48c9c7103f9
Julian Bustos <julian.m.bustos@gmail.com>
2025-05-07 14:59:49 -0400

    fixes the hst and totals in the admin create booking page

--- app/admin/dashboard/markets/[id]/create-booking/SelectDetails.tsx
+++ app/admin/dashboard/markets/[id]/create-booking/SelectDetails.tsx
@@ -1,14 +1,27 @@
-"use client"
-import { TDayWithTable, TSanityMarket, TTableInDay } from "@/sanity/queries/admin/markets/zods";
+"use client";
+import {
+  TDayWithTable,
+  TSanityMarket,
+  TTableInDay,
+} from "@/sanity/queries/admin/markets/zods";
 import { TUserWithOptionalBusinessRef } from "@/zod/user-business";
 import { useState } from "react";
-import { TDateType, TSelectedTableType } from "@/app/dashboard/markets/[id]/select-preferences/_components/SelectOptions";
+import {
+  TDateType,
+  TSelectedTableType,
+} from "@/app/dashboard/markets/[id]/select-preferences/_components/SelectOptions";
 import SelectPaymentOptions from "./SelectPaymentOptions";
 import SelectDates from "./SelectDates";
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from "next/navigation";
 import Button from "@/app/_components/Button";
 
-const SelectDetails = ({ market, user }: { market: TSanityMarket, user: TUserWithOptionalBusinessRef }) => {
+const SelectDetails = ({
+  market,
+  user,
+}: {
+  market: TSanityMarket;
+  user: TUserWithOptionalBusinessRef;
+}) => {
   const [newSelectedDates, setNewSelectedDates] = useState<TDayWithTable[]>([]);
   const [selectedTables, setSelectedTables] = useState<TSelectedTableType[]>(
     []
@@ -16,14 +29,15 @@ const SelectDetails = ({ market, user }: { market: TSanityMarket, user: TUserWit
   const [isPayNowSelected, setIsPayNowSelected] = useState<boolean>(true);
   const searchParams = useSearchParams();
 
-  const selectedBusinessCategory = searchParams.get('businessCategory')
-  const selectedVendor = searchParams.get('selectedVendor')
-
+  const selectedBusinessCategory = searchParams.get("businessCategory");
+  const selectedVendor = searchParams.get("selectedVendor");
 
   const handleNewDateSelect = (date: TDayWithTable) => {
-    if (newSelectedDates.some(d => d.date === date.date)) {
+    if (newSelectedDates.some((d) => d.date === date.date)) {
       setNewSelectedDates((prev) => prev.filter((d) => d.date !== date.date));
-      setSelectedTables((prevTables) => prevTables.filter((t) => t.date !== date.date));
+      setSelectedTables((prevTables) =>
+        prevTables.filter((t) => t.date !== date.date)
+      );
     } else {
       setNewSelectedDates((prev) => [...prev, date]);
     }
@@ -51,7 +65,6 @@ const SelectDetails = ({ market, user }: { market: TSanityMarket, user: TUserWit
 
   if (!selectedVendor) return;
 
-
   return (
     <>
       <SelectDates
@@ -59,33 +72,53 @@ const SelectDetails = ({ market, user }: { market: TSanityMarket, user: TUserWit
         handleDateSelect={handleNewDateSelect}
         selectedDates={newSelectedDates}
         handleOnTableChange={handleOnTableChange}
-        businessCategory={selectedBusinessCategory ?? ''}
+        businessCategory={selectedBusinessCategory ?? ""}
       />
 
-      <SelectPaymentOptions
-        setIsPayNowSelected={setIsPayNowSelected}
-      />
+      <SelectPaymentOptions setIsPayNowSelected={setIsPayNowSelected} />
 
-      <h2 className="font-bold">Total Booking Cost:</h2>
+      <h2 className="font-bold">Subtotal:</h2>
       <span>${totalToPay}</span>
       <input hidden type="text" value={totalToPay} name="total" readOnly />
 
-      <h2 className="font-bold">Total Collected:</h2>
-      <span>${dueNow}</span>
-      <input hidden type="text" value={dueNow} name="paid" readOnly />
+      <h2 className="font-bold">Total:</h2>
+      <span>${(+dueNow + dueNow * 0.13).toFixed(2)}</span>
+      <input
+        hidden
+        type="text"
+        value={(+dueNow + dueNow * 0.13).toFixed(2)}
+        name="paid"
+        readOnly
+      />
       <h2 className="font-bold">HST:</h2>
-      <span>${(dueNow * .13).toFixed(2)}</span>
-      <input hidden type="text" value={(dueNow * .13).toFixed(2)} name="hst" readOnly />
+      <span>${(dueNow * 0.13).toFixed(2)}</span>
+      <input
+        hidden
+        type="text"
+        value={(dueNow * 0.13).toFixed(2)}
+        name="hst"
+        readOnly
+      />
 
       <p>Amount Owing:</p>
       <span>${totalToPay - dueNow}</span>
-      <input hidden type="text" value={totalToPay - dueNow} name="owed" readOnly />
+      <input
+        hidden
+        type="text"
+        value={totalToPay - dueNow}
+        name="owed"
+        readOnly
+      />
       <input hidden type="text" value={market._id} name="marketId" readOnly />
 
-      <Button className="rounded-none bg-black text-white py-4 px-6 text-lg mx-auto my-10" type="submit">Complete Booking</Button>
-
+      <Button
+        className="rounded-none bg-black text-white py-4 px-6 text-lg mx-auto my-10"
+        type="submit"
+      >
+        Complete Booking
+      </Button>
     </>
   );
-}
+};
 
 export default SelectDetails;
